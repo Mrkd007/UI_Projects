05cd590de2177a15eb35ff322255c5338b9e3612
Mrkd007 <itskd17@gmail.com>
2022-01-23 12:28:18 +0530

    Update: added new clock project

---
+++ analogWatch/script.js
@@ -0,0 +1,17 @@
+const step = () => {
+    let time = new Date();
+    let hrs = time.getHours(), min = time.getMinutes(), sec = time.getSeconds();
+    hrs = hrs > 12 ? (hrs - 12) : hrs;
+    let secAngle = sec * 6 || 0;
+    let minAngle = min * 6 + parseInt(secAngle/60);
+    let hrsAngle = hrs * 30 + parseInt(minAngle/12);
+    let secElm = document.querySelector('span.second');
+    secElm.style.transform = 'rotate(' + secAngle + 'deg)';
+    let minElm = document.querySelector('span.minute');
+    minElm.style.transform = 'rotate(' + minAngle + 'deg)';
+    let hrsElm = document.querySelector('span.hour');
+    hrsElm.style.transform = 'rotate(' + hrsAngle + 'deg)';
+
+    window.requestAnimationFrame(step);
+}asdasdasdasdas
+window.requestAnimationFrame(step);
